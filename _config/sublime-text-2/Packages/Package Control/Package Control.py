86fe4c376dddacb622a0c0e18a405fe0adaf120f
Nicolas Hennion <nicolas@nicolargo.com>
2013-12-24 14:52:49 +0100

    Update aliases

--- _config/sublime-text-2/Packages/Package Control/Package Control.py
+++ _config/sublime-text-2/Packages/Package Control/Package Control.py
@@ -1027,6 +1027,8 @@ class PackageProvider(PlatformComparator):
                 download['url'] = re.sub(
                     '^(https://nodeload.github.com/[^/]+/[^/]+/)zipball(/.*)$',
                     '\\1zip\\2', download['url'])
+                download['url'] = download['url'].replace(
+                    'nodeload.github.com', 'codeload.github.com')
                 rewritten_downloads.append(download)
 
             info = {
@@ -1148,10 +1150,10 @@ class GitHubPackageProvider(NonCachingProvider):
         if commit_info == False:
             return False
 
-        # We specifically use nodeload.github.com here because the download
+        # We specifically use codeload.github.com here because the download
         # URLs all redirect there, and some of the downloaders don't follow
         # HTTP redirect headers
-        download_url = 'https://nodeload.github.com/' + \
+        download_url = 'https://codeload.github.com/' + \
             repo_info['owner']['login'] + '/' + \
             repo_info['name'] + '/zip/' + urllib.quote(branch)
 
@@ -1251,10 +1253,10 @@ class GitHubUserProvider(NonCachingProvider):
                 'downloads': [
                     {
                         'version': utc_timestamp,
-                        # We specifically use nodeload.github.com here because
+                        # We specifically use codeload.github.com here because
                         # the download URLs all redirect there, and some of the
                         # downloaders don't follow HTTP redirect headers
-                        'url': 'https://nodeload.github.com/' + \
+                        'url': 'https://codeload.github.com/' + \
                             package_info['owner']['login'] + '/' + \
                             package_info['name'] + '/zip/master'
                     }
@@ -1411,11 +1413,11 @@ class Downloader():
 
         cert_info = certs_list.get(domain)
         if cert_info:
-            cert_match = self.locate_cert(certs_dir, cert_info[0], cert_info[1])
+            cert_match = self.locate_cert(certs_dir, cert_info[0], cert_info[1], domain, timeout)
 
         wildcard_info = certs_list.get('*')
         if wildcard_info:
-            cert_match = self.locate_cert(certs_dir, wildcard_info[0], wildcard_info[1]) or cert_match
+            cert_match = self.locate_cert(certs_dir, wildcard_info[0], wildcard_info[1], domain, timeout) or cert_match
 
         if not cert_match:
             print '%s: No CA certs available for %s.' % (__name__, domain)
@@ -1423,7 +1425,7 @@ class Downloader():
 
         return ca_bundle_path
 
-    def locate_cert(self, certs_dir, cert_id, location):
+    def locate_cert(self, certs_dir, cert_id, location, domain, timeout):
         """
         Makes sure the SSL cert specified has been added to the CA cert
         bundle that is present on the machine
@@ -1448,7 +1450,7 @@ class Downloader():
         if not os.path.exists(cert_path):
             if str(location) != '':
                 if re.match('^https?://', location):
-                    contents = self.download_cert(cert_id, location)
+                    contents = self.download_cert(cert_id, location, domain, timeout)
                 else:
                     contents = self.load_cert(cert_id, location)
                 if contents:
@@ -1457,7 +1459,7 @@ class Downloader():
             return False
         return True
 
-    def download_cert(self, cert_id, url):
+    def download_cert(self, cert_id, url, domain, timeout):
         """
         Downloads CA cert(s) from a URL
 
@@ -2666,7 +2668,9 @@ class PackageManager():
             return False
 
         url = url.replace(' ', '%20')
-        hostname = urlparse.urlparse(url).hostname.lower()
+        hostname = urlparse.urlparse(url).hostname
+        if hostname:
+            hostname = hostname.lower()
         timeout = self.settings.get('timeout', 3)
 
         rate_limited_cache = _channel_repository_cache.get('rate_limited_domains', {})
@@ -2816,10 +2820,19 @@ class PackageManager():
                     if provider.get_packages(repo) == False:
                         continue
                     packages_cache_key = repo + '.packages'
+
+                    # Handle the transition from nodeload to codeload
+                    channel_packages = provider.get_packages(repo)
+                    for _name in channel_packages:
+                        package_info = channel_packages[_name]
+                        for download in package_info['downloads']:
+                            download['url'] = download['url'].replace(
+                                'nodeload.github.com', 'codeload.github.com')
+
                     _channel_repository_cache[packages_cache_key] = {
                         'time': time.time() + self.settings.get('cache_length',
                             300),
-                        'data': provider.get_packages(repo)
+                        'data': channel_packages
                     }
 
                 # Have the local name map override the one from the channel
@@ -2946,6 +2959,14 @@ class PackageManager():
             if repository_packages == False:
                 continue
             cache_key = downloader.repo + '.packages'
+
+            # Handle the transition from nodeload to codeload
+            for _name in repository_packages:
+                package_info = repository_packages[_name]
+                for download in package_info['downloads']:
+                    download['url'] = download['url'].replace(
+                        'nodeload.github.com', 'codeload.github.com')
+
             _channel_repository_cache[cache_key] = {
                 'time': time.time() + self.settings.get('cache_length', 300),
                 'data': repository_packages
@@ -4382,18 +4403,19 @@ class RemovePackageCommand(sublime_plugin.WindowCommand,
             return
         package = self.package_list[picked][0]
 
+        settings = sublime.load_settings(preferences_filename())
+        ignored = settings.get('ignored_packages')
+        if not ignored:
+            ignored = []
+
         # Don't disable Package Control so it does not get stuck disabled
         if package != 'Package Control':
-            settings = sublime.load_settings(preferences_filename())
-            ignored = settings.get('ignored_packages')
-            if not ignored:
-                ignored = []
             if not package in ignored:
                 ignored.append(package)
                 settings.set('ignored_packages', ignored)
                 sublime.save_settings(preferences_filename())
+            ignored.remove(package)
 
-        ignored.remove(package)
         thread = RemovePackageThread(self.manager, package,
             ignored)
         thread.start()
